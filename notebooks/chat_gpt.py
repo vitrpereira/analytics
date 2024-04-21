6e1a60a2111abe3a3c63ae79946fe9efe8d92048
Vitor Pereira <vitorpereira@MacBook-Air-de-Vitor.local>
2024-04-21 10:57:47 -0300

    delete unused files

--- notebooks/chat_gpt.py
+++
@@ -1,9 +0,0 @@
-import openai 
-
-openai.api_key = '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'
-
-openai.Completion.create(
-    model = 'text-davinci-003',
-    prompt = "Explain the cicle of fifth's"
-)
-
